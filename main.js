40d0aab4ee958bea3406155b7d062033c559c154
randi089 <randifebriadi@gmail.com>
2024-07-26 13:54:26 +0700

    edit dikit

--- main.js
+++ main.js
@@ -1,19 +1,39 @@
-const btn = document.querySelector('.btn');
-const h1 = document.querySelector('h1');
+// const btn = document.querySelector('.btn');
+// const h1 = document.querySelector('h1');
 
-btn.addEventListener('click', function() {
-    h1.setAttribute('id', 'mantap');
-});
+// btn.addEventListener('click', function() {
+//     h1.setAttribute('id', 'mantap');
+// });
 
-const img = document.querySelector('.img');
-const bar = document.querySelector('.bar');
-let width = 100;
+// const img = document.querySelector('.img');
+// const bar = document.querySelector('.bar');
+// let width = 100;
 
-img.addEventListener('click', function() {
-        width = width-10;
-        bar.style.width = `${width}%`;
-        if(width == 0) {
-            confirm('You Win, Again?');
-            location.reload();            
-        }      
-});
+// img.addEventListener('click', function() {
+//         width -= 10;
+//         bar.style.width = `${width}%`;
+//         if(width == 0) {
+//             confirm('You Win, Again?');
+//             location.reload();
+//         }
+// });
+
+// console.log(0.1+0.2);
+
+// document.addEventListener("click", function (e) {
+// if (e.target.id == "tes1") {
+// Benda.classList.toggle("tunjukkan");
+// e.target.textContent = e.target.textContent == "Toggle" ? "Toggled" : "Toggle";
+// }
+// else if (e.target.id == "namaidnya") {
+// Benda.classList.toggle("tunjukkan");
+//   e.target.textContent = e.target.textContent == "Toggle" ? "Toggled" : "Toggle";
+// }
+// });
+
+// const sum = (num1, num2) => num1 + num2;
+
+function sum(num1, num2) {
+  return num1 + num2;
+}
+console.log(sum(1, 2));
